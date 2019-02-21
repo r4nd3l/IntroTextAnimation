462cb41414f2588c9b23657ef2d1ab675d9855fb
matemolnar88 <matemolnar88@gmail.com>
2019-02-21 16:10:12 +0100

    First  commmit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// Default DevCorner JavaScript Setting
+$(function(){
+  var welcomeSection = $('.welcome-section'),
+    enterButton = welcomeSection.find('.enter-button');
+
+    setTimeout(function(){
+      welcomeSection.removeClass('content-hidden');
+    }, 800);
+
+    enterButton.on('click', function(e){
+      e.preventDefault();
+      welcomeSection.addClass('content-hidden').fadeOut();
+    });
+});
---
+++ readme.md
@@ -0,0 +1,4 @@
+Html, Css and jQuery based Intro Text Animation website - exercise
+---
+
+![IntroTextAnimation](https://github.com/r4nd3l/IntroTextAnimation/blob/master/img/sample.gif)
